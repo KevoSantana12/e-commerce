120cce84adb2e090dba48719dbf6158dae0c0327
Kevin Santana <klinarte258@gmail.com>
2024-11-24 11:23:36 -0700

    email sent it

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -6,4 +6,20 @@ urlpatterns = [
 
     path('register', views.register, name='register'),
 
+
+    # Email verification URL' s
+
+    path('email-verification/<str:uidb64>/<str:token>/', views.email_verification, name='email-verification'),
+
+
+    path('email-verification-sent', views.email_verification_sent, name='email-verification-sent'),
+
+
+    path('email-verification-failed', views.email_verification_failed, name='email-verification-failed'),
+
+
+    path('email-verification-success', views.email_verification_success, name='email-verification-success'),
+
+
+
 ]
